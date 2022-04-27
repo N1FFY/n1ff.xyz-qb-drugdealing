0b78556ef92efd8c6d2d0f3626debed0c5e2c5dd
Niff <68659554+N1FFY@users.noreply.github.com>
2022-04-28 00:57:34 +0100

    fxmanifest.lua update + client side update moneywash

--- client/c_moneywash.lua
+++ client/c_moneywash.lua
@@ -2,13 +2,28 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('qb-drugdealing:client:startMoneyWash')
 AddEventHandler('qb-drugdealing:client:startMoneyWash', function()
-    QBCore.Functions.TriggerCallback('niff-selldrugs:server:getAvailableDrugs', function(result)
+    QBCore.Functions.TriggerCallback('qb-drugdealing:server:getMoneyWashList', function(result)
         MoneyWashList = result
         if MoneyWashList ~= nil then
             MoneyWashType = math.random(1, #MoneyWashList)
             MoneyWashAmount = math.random(Config.MinSaleAmount, Config.MaxSaleAmount)
-            MoneyWashItem = availableDrugs[drugType]
+            MoneyWashItem = MoneyWashList[MoneyWashType]
             MoneyWashData = Config.DrugsPrice[currentOfferDrug.item]
-            AmountToWash = math.random(ddata.min, ddata.max) * bagAmount
+            PriceOfWashed = math.random(MoneyWashData.min, MoneyWashData.max) * bagAmount
         end
     end)
+    if MoneyWashList ~= nil then
+        PlayerPed = PlayerPedId()
+        ClearPedTasksImmediately(PlayerPed)
+        TaskPlayAnim( PlayerPed, "anim@gangops@facility@servers@", "hotwire", 8.0, 1.0, -1, 16, 0, 0, 0, 0 )
+        QBCore.Functions.Progressbar("Moneywash", "Starting the Moneywash Process..", Config.SaleTime*1000, false, true, {
+            disableMovement = false,
+            disableCarMovement = false,
+            disableMouse = false,
+            disableCombat = true,
+        }, {}, {}, {}, function()
+        end)
+        TriggerServerEvent('qb-drugdealing:server:ExchangeMoneyItems', MoneyWashList[MoneyWashType].item, MoneyWashAmount, PriceOfWashed)
+    end
+end)
+        
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'Niff-SellDrugs'
+description 'qb-drugdealing'
+author 'N1FF'
 version '1.0.0'
 
-shared_scripts {
-	'config.lua'
-}
-
-server_script 'server.lua'
-
-client_scripts {
-    'client.lua'
-}
+shared_script 'config.lua'
+server_script 'server/*.lua'
+client_script 'client/*.lua'
 
 lua54 'yes'
